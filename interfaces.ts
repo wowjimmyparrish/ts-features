170da886d2b664a52446b9ead4d8441127b5977b
Jimmy <jparrish@citytelecoin.com>
2024-01-08 13:01:35 -0600

    update

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,28 @@
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`
+  }
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`)
-  console.log(`Year: ${vehicle.year}`)
-  console.log(`Broken: ${vehicle.broken}`)
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`
+  }
 }
 
-printVehicle(oldCivic)
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary())
+}
+
+printSummary(oldCivic)
+printSummary(drink)
